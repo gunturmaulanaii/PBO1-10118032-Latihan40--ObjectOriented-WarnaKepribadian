175547578a414d10e4ef6b5c529aa23fcf5938cc
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-16 22:27:20 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan40-(ObjectOriented)WarnaKepribadian/src/pbo1/pkg10118032/latihan40/objectoriented/warnakepribadian/PBO110118032Latihan40ObjectOrientedWarnaKepribadian.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan40.objectoriented.warnakepribadian;
+
+/**
+ *
+ * @author Guntur Maulana I
+ */
+public class PBO110118032Latihan40ObjectOrientedWarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
